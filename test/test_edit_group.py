15aa5a11f6c6abafc777fe3a964838a24a9ff38a
Tatyana S <tana@example.com>
2020-02-26 21:15:55 +0700

    Добавлены тесты для удаления, редактирования для групп и контактов

---
+++ test/test_edit_group.py
@@ -0,0 +1,7 @@
+from model.group import Group
+
+
+def test_edit_first_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_first(Group(name="edited_name", header="edited_header", footer="edited_footer"))
+    app.session.logout()
