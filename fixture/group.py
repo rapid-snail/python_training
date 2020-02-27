3663297d4dd0a39646411840898585e87d3b3000
Tatyana S <tana@example.com>
2020-02-27 22:28:19 +0700

    Для групп и контактов выделены методы заполнения формы

--- fixture/group.py
+++ fixture/group.py
@@ -13,15 +13,7 @@ class GroupHelper:
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_fields(group)
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
@@ -47,12 +39,16 @@ class GroupHelper:
         # submit edition
         wd.find_element_by_name("edit").click()
         # edit fields
+        self.fill_fields(group)
+        # submit edition
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+
+    def fill_fields(self, group):
+        wd = self.app.wd
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").clear()
         wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit edition
-        wd.find_element_by_name("update").click()
-        self.return_to_groups_page()
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -3,7 +3,7 @@ from model.contact import Contact
 
 def test_edit_first_contact(app):
     app.session.login(username="admin", password="secret")
-    app.contact.edit_first(Contact(firstname="first name edited", middlename="middle name", lastname="last name", nickname="nickname", title="title",
-                               company="company", address="address", homephone="9999999999", mobile="8888888888", workphone="5555555555", fax="333333", email="email@mail.ru",
-                               email2="email2@mail.ru", email3="email3@mail.ru", homepage="http://homepage", address2="address2", phone2="2222222222", notes="notes"))
+    app.contact.edit_first(Contact(firstname="first name edited", middlename="middle name edited", lastname="last name edited", nickname="nickname edited", title="title edited",
+                               company="company edited", address="address edited", homephone="9999999999", mobile="8888888888", workphone="5555555555", fax="333333", email="email@mail.ru",
+                               email2="email2@mail.ru", email3="email3@mail.ru", homepage="http://homepage", address2="address2", phone2="2222222222", notes="notes edited"))
     app.session.logout()
